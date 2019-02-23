40f320a0aa699e7ee8445eef7b3e90e102df5af2
Alexey Pekov <apekoff@gmail.com>
2019-02-23 12:03:08 +0300

    upgrade gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,64 @@
-var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  notify = require('gulp-notify'),
-  cleancss = require('gulp-clean-css'),
-  autoprefixer = require('gulp-autoprefixer'),
-  pug = require('gulp-pug');
-browserSync = require('browser-sync');
-
-gulp.task('browser-sync', function() {
-  browserSync({
+const autoprefixer = require("gulp-autoprefixer");
+const browsersync = require("browser-sync").create();
+const del = require("del");
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const sass = require("gulp-sass");
+const pug = require("gulp-pug");
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
     server: {
-      baseDir: 'build/app',
+      baseDir: "build/app",
       serveStaticOptions: {
         extensions: ['html']
       }
     },
-    notify: false,
     open: false
-    // online: false, // Work Offline Without Internet Connection
-    // tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
   });
-});
+  done();
+}
 
-gulp.task('pug', function() {
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
+}
+
+
+// Clean assets
+function clean() {
+  return del(["./build/*"]);
+}
+
+// Optimize Images
+function images() {
+  return gulp
+    .src("./app/img/*")
+    .pipe(newer("./build/app/img/.."))
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            {
+              removeViewBox: false,
+              collapseGroups: true
+            }
+          ]
+        })
+      ])
+    )
+    .pipe(gulp.dest("./build/app/img/"));
+}
+
+
+
+// Build pug
+function html() {
   return gulp
     .src([
       'app/pug/**/*.pug',
@@ -37,23 +74,13 @@ gulp.task('pug', function() {
       process.stderr.write(err.message + '\n');
       this.emit('end');
     })
-    .pipe(gulp.dest('build/app'));
-});
+    .pipe(gulp.dest('./build/app'));
+}
 
-gulp.task('layouts', function() {
+
+// CSS task
+function css() {
   return gulp
-    .src(['app/pug/layouts/*.pug', 'app/pug/partials/*.pug'])
-    .pipe(
-      pug({
-        pretty: true
-      })
-    )
-    .pipe(gulp.dest('build/_layouts'));
-});
-
-gulp.task('styles', function() {
-  return (
-    gulp
       .src([
         'node_modules/bootstrap/dist/css/bootstrap.min.css',
         'app/sass/**/*.sass'
@@ -62,12 +89,13 @@ gulp.task('styles', function() {
       // .pipe(rename({ suffix: '.min', prefix: '' }))
       .pipe(autoprefixer(['last 15 versions']))
       .pipe(cleancss({ level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
-      .pipe(gulp.dest('build/app/css'))
+      .pipe(gulp.dest('./build/app/css'))
       .pipe(browserSync.stream())
-  );
-});
+}
 
-gulp.task('js', function() {
+
+// Transpile, concatenate and minify scripts
+function js() {
   return (
     gulp
       .src([
@@ -77,19 +105,35 @@ gulp.task('js', function() {
       ])
       // .pipe(concat('scripts.min.js'))
       // .pipe(uglify()) // Mifify js (opt.)
-      .pipe(gulp.dest('build/app/js'))
+      .pipe(gulp.dest('./build/app/js'))
       .pipe(browserSync.reload({ stream: true }))
   );
-});
+}
 
-gulp.task('rebuild', ['pug'], function() {
-  browserSync.reload();
-});
+// Watch files
+function watchFiles() {
+  gulp.watch("./app/sass/**/*.sass", css);
+  gulp.watch("./app/js/**/*.js*", js);
+  gulp.watch(
+    [
+      'app/pug/**/*.pug',
+      '!app/pug/layouts/**/*.pug',
+      '!app/pug/partials/**/*.pug'
+    ],
+    gulp.series(html, browserSyncReload)
+  );
+  gulp.watch("./app/img/*", images);
+}
+// define complex tasks
+const build = gulp.series(clean, gulp.parallel(css, images, html, js));
+const watch = gulp.parallel(watchFiles, browserSync);
 
-gulp.task('watch', ['styles', 'js', 'pug', 'browser-sync'], function() {
-  gulp.watch('app/sass/**/*.sass', ['styles']);
-  gulp.watch(['app/js/*.js'], ['js']);
-  gulp.watch('app/pug/**/*.pug', ['rebuild']);
-});
-
-gulp.task('default', ['watch']);
+// export tasks
+exports.images = images;
+exports.css = css;
+exports.js = js;
+exports.html = html;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
